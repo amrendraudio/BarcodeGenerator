6a11d8d2a7c33d13e726a1ec6f041201954cc38e
Amrendra Singh <Amrendra Singh@AMSI-BLR-01.Dlink>
2017-02-10 08:18:01 +0530

    Committing Junit Changes

---
+++ BarcodeGenerator/src/main/java/org/amrendra/barcode/services/impl/BarcodeImpl.java
@@ -0,0 +1,15 @@
+package org.amrendra.barcode.services.impl;
+
+import org.amrendra.barcode.services.Barcode;
+
+public class BarcodeImpl implements Barcode
+{
+
+    @Override
+    public String generateBarcode( String uniqueId )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- BarcodeGenerator/src/test/java/org/amrendra/barcode/services/test/TestBarcode.java
+++ BarcodeGenerator/src/test/java/org/amrendra/barcode/services/test/TestBarcode.java
@@ -1,7 +1,23 @@
 package org.amrendra.barcode.services.test;
 
+import org.amrendra.barcode.services.Barcode;
+import org.amrendra.barcode.services.impl.BarcodeImpl;
+import org.junit.Test;
+
 public class TestBarcode
 {
     
-
+    /**
+     * 
+     */
+    @Test
+    public void testBarcodeBlank( )
+    {
+       Barcode barcode = new BarcodeImpl();
+       String uniqueId = "";
+       barcode.generateBarcode( uniqueId );
+       
+       
+    }
+    
 }
